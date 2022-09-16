3d8c8337195c9efe6a6e3afbafc434ebfc9c44d2
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2022-09-16 16:14:22 +0200

    ...

--- deviveroo/screens/HomeScreen.js
+++ deviveroo/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 //import liraries
 import { useNavigation } from '@react-navigation/native';
-import React, { Component, useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import { View, Text, SafeAreaView, Image } from 'react-native';
 import {
     UserIcon,
@@ -22,15 +22,15 @@ const HomeScreen = () => {
     return (
         <SafeAreaView className='bg-white pt-5'>
             <Text className='text-red-500'>
+
                 {/* Header */}
-                <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+                <View className='flex-row pb-3 items-center mx-4 space-x-2' >
                     <Image 
                         source={{
                             uri: 'https://links.papareact.com/wru'
                         }}
                         className='h-7 w-7 bg-gray-300 p-4 rounded-full'
                     />
-
                     <View className='flex-1' >
                         <Text className='font-bold text-gray-400 text-xs'>
                             Deliver Now!
