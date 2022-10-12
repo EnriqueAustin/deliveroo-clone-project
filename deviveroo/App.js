7bd618cab1fb1185feb9ed2a096a355461c87998
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2022-10-12 13:46:20 +0200

    created basket screen

--- deviveroo/App.js
+++ deviveroo/App.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ResturantScreen from './screens/ResturantScreen';
+import BasketScreen from './screens/BasketScreen';
 
 import { NativeWindStyleSheet } from "nativewind";
 import { Provider } from 'react-redux';
@@ -20,6 +21,9 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name='Home' component={HomeScreen} />
           <Stack.Screen name='Restaurant' component={ResturantScreen} />
+          <Stack.Screen name='Basket' component={BasketScreen} 
+            options={{ presentation: 'modal', headerShown: false }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
---
+++ deviveroo/screens/BasketScreen.js
@@ -0,0 +1,15 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// create a component
+const BasketScreen = () => {
+    return (
+        <View>
+            <Text>BasketScreen</Text>
+        </View>
+    );
+};
+
+//make this component available to the app
+export default BasketScreen;
